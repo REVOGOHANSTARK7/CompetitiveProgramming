b6deb906e2b22b700f2d68725fceacb18da6c720
REVOGOHANSTARK7 <72118470+REVOGOHANSTARK7@users.noreply.github.com>
2021-07-13 12:34:27 +0530

    Day2_4

--- 07-isfactor-Python/isfactor.py
+++ 07-isfactor-Python/isfactor.py
@@ -6,4 +6,15 @@
 
 
 def fun_isfactor(f, n):
-	return False # replace with your solution
+  f=abs(f)
+  n=abs(n)
+  if(f==0 and n==0):
+    return True
+  elif(n==0):
+    return True
+  elif(f==0):
+    return False
+  elif(n%f==0):
+    return True
+  else:
+    return False # replace with your solution
