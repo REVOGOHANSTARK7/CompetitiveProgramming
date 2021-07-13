c6f54a2e6569ca0e2a866d822dfdd35e63ceb182
REVOGOHANSTARK7 <72118470+REVOGOHANSTARK7@users.noreply.github.com>
2021-07-13 15:40:12 +0530

    Day2_2

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -7,5 +7,30 @@
 
 
 def fun_set_kth_digit(n, k, d):
-		return 0
+  if(n>0):
+    m=True
+  else:
+    m=False
+  count=0
+  n=abs(n)
+  a=0
+  b=0
+  while(n>0):
+    s=n%10
+    if(count!=k):
+      b=b+(10**a*s)
+    else:
+      b=b+(10**a*d)
+    n//=10
+    count+=1
+    a+=1
+  if(count==k):
+    b=b+(10**a*d)
+      
+  if(m==True):
+    return b
+  else:
+    return (-b)
+      
+      
 
