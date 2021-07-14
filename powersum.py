6b94b9ed9591f74eb3e31b02cb862e94221a6d5e
REVOGOHANSTARK7 <72118470+REVOGOHANSTARK7@users.noreply.github.com>
2021-07-14 13:14:12 +0530

    Day3_8

--- powersum.py
+++ powersum.py
@@ -10,10 +10,26 @@
 # If n is negative, return 0. Similarly, if k is negative, 
 # return 0.
 
+def power(x, y):
+    res=1
+    while(y>0):
+        if((y & 1) == 1) :
+            res=res*x
+        y=y>>1
+        x=x*x
+    return res
+
 def powerSum(n, k):
-    # Your code goes here...
-    return 0
-
-# Write your own test cases here...
-
+    sum=0
+    if(n<0 or k<0):
+        return 0
+    else:
+        for i in range(1,n+1):
+            sum+=power(i,k)
+        return sum
+        
+assert(powerSum(0,0) == 0)
+assert(powerSum(2,10) == 1025)
+assert(powerSum(10,2) == 385)
+assert(powerSum(3,10) == 60074)
 print ("All test cases passed...")
