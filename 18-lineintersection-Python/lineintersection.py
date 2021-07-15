18bd4d7e5acd47536ab26bb224e79d119cdf53d5
REVOGOHANSTARK7 <72118470+REVOGOHANSTARK7@users.noreply.github.com>
2021-07-15 13:43:46 +0530

    DAy5_1

--- 18-lineintersection-Python/lineintersection.py
+++ 18-lineintersection-Python/lineintersection.py
@@ -2,7 +2,8 @@
 # Write the function lineintersection(m1, b1, m2, b2) that takes four int or float values representing the 2 lines:
 #     y = m1*x + b1
 #     y = m2*x + b2
-# This function returns the x value of the point of intersection of the two lines. If the lines are parallel, or identical, the function should return None.
+# This function returns the x value of the point of intersection of the two lines. If the lines are parallel, or identical, the function should 
+# return None.
 
 def lineintersection(m1, b1, m2, b2):
 	# your code goes here
