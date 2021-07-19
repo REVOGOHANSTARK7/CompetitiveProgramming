5687bacac2dc3e0d71c9a3b286909667cfb2d6d4
REVOGOHANSTARK7 <72118470+REVOGOHANSTARK7@users.noreply.github.com>
2021-07-19 19:41:47 +0530

    Day8_3

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -5,4 +5,45 @@
 
 
 def fun_nth_happy_prime(n):
-	return 0
+  found=0
+  got=0
+  while(found<=n):
+    got+=1
+    if(ishappy(got) and isaprime(got)):
+      found+=1
+  return got
+
+def isaprime(x):
+  if(x<2):
+    return False
+  if(x==2):
+    return True
+  if(x%2==0):
+    return False
+  maximfactor=round(x**0.5)
+  for k in range(3,maximfactor+1,2):
+    if(x%k==0):
+      return False
+  return True
+
+def sumofsquares(n):
+  sumofsquare=0
+  while(n):
+    sumofsquare=sumofsquare+((n%10)*(n%10))
+    n=int(n/10)
+  return sumofsquare
+
+def ishappy(n):
+  start=n
+  stop=n
+  while(True):
+    start=sumofsquares(start)
+    stop=sumofsquares(sumofsquares(stop))
+    if(start!=stop):
+      continue;
+    else:
+      break;
+  return (start==1)
+
+
+  
