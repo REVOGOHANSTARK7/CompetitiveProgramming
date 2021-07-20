595b4f7d5515d6aeeddf8afc95460e014ec9a5ef
REVOGOHANSTARK7 <72118470+REVOGOHANSTARK7@users.noreply.github.com>
2021-07-20 20:31:54 +0530

    Day9_4

--- 08-nth_additive_prime-Python/nth_additive_prime.py
+++ 08-nth_additive_prime-Python/nth_additive_prime.py
@@ -3,8 +3,35 @@
 # the sum of its digits is also prime. For example, 113 is prime and 1+1+3==5 and 5 
 # is also prime, so 113 is an Additive Prime. fun_nth_additive_prime(0) returns 2
 
+import math
 
+def sums(n):
+  n=abs(n)
+  tot=0
+  while(n>0):
+    tot+=(n%10)
+    n//=10
+  return tot
 
+def prime(n):
+  if(n<2 or n%2==0):
+    return False
+  elif(n==2):
+    return True
+  fac=round(math.sqrt(n))
+  for i in range(3,fac+1,2):
+    if(n%i==0):
+      return False
+  return True
+
+def additive(n):
+  return (prime(n) and prime(sums(n)))
 
 def fun_nth_additive_prime(n):
-	return 1
+  found=0
+  got=0
+  while(found<=n):
+    got+=1
+    if(additive(got)):
+      found+=1
+  return got
