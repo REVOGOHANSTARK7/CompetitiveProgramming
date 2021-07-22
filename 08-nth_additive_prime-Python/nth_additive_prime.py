31a4419a3a10896f55846fd21a0eb48db558027e
REVOGOHANSTARK7 <72118470+REVOGOHANSTARK7@users.noreply.github.com>
2021-07-22 11:55:08 +0530

    commit

--- 08-nth_additive_prime-Python/nth_additive_prime.py
+++ 08-nth_additive_prime-Python/nth_additive_prime.py
@@ -35,3 +35,45 @@ def fun_nth_additive_prime(n):
     if(additive(got)):
       found+=1
   return got
+
+# import math
+
+# def sums(n):
+#   n=abs(n)
+#   tot=0
+#   while(n>0):
+#     tot+=(n%10)
+#     n//=10
+#   return tot
+
+# def prime(n):
+#   if(n==2):
+#     return True
+#   elif(n<2 or n%2==0):
+#     return False
+  
+#   fac=round(math.sqrt(n))
+#   for i in range(3,fac+1,2):
+#     if(n%i==0):
+#       return False
+#   return True
+
+# def isAdditivePrime(n):
+#   if(prime(n) and prime(sums(n))):
+#     return True
+#   else:
+#     return False
+  
+
+# assert (isAdditivePrime(3) == True)
+# assert (isAdditivePrime(5) == True)
+# assert (isAdditivePrime(13) == False)
+# assert (isAdditivePrime(23) == True)
+# assert (isAdditivePrime(29) == True)
+# assert (isAdditivePrime(41) == True)
+# assert (isAdditivePrime(98) == False)
+# assert (isAdditivePrime(198) == False)
+# assert (isAdditivePrime(290) == False)
+# assert (isAdditivePrime(67) == True)
+# assert (isAdditivePrime(97) == False)
+# print("All test cases passed... :-)")
