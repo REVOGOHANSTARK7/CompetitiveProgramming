acb7f437b6376032020ddccecf82a6905c09fcea
REVOGOHANSTARK7 <72118470+REVOGOHANSTARK7@users.noreply.github.com>
2021-07-22 12:29:20 +0530

    Day9_5

--- 01-linkedlist-Python/linkedlist.py
+++ 01-linkedlist-Python/linkedlist.py
@@ -18,26 +18,63 @@ class LinkedList(object):
         self.head = head
         
     def append(self, new_element):
-        # Your code goes here
-        pass
+        c=self.head
+        if(self.head):
+            while(c.next):
+                c=c.next
+            c.next=new_element
+        else:
+            self.head=new_element
             
+          
     def get_position(self, position):
+        current=self.head
+        count=1
+        if(position<1):
+            return None
+        while(current and count<=position):
+            if(count==position):
+                return current
+            current=current.next
+            count+=1
+        return current
         """Get an element from a particular position.
         Assume the first position is "1".
         Return "None" if position is not in the list."""
-        # Your code goes here
-        pass
-    
+       
     def insert(self, new_element, position):
+        count=1
+        current=self.head
+        if(position<1):
+            return None
+        elif(position==1):
+            new_element.next=self.head
+            self.head=new_element
+        else:
+            while(current and count<position):
+                if(count==position-1):
+                    new_element.next=current.next
+                    current.next=new_element
+                current=current.next
+                count+=1
         """Insert a new node at the given position.
         Assume the first position is "1".
         Inserting at position 3 means between
         the 2nd and 3rd elements."""
-        # Your code goes here
-        pass
+        
     
     
     def delete(self, value):
         """Delete the first node with a given value."""
         # Your code goes here
-        pass
+        current=self.head
+        before=None
+        while(current.value!=value and current.next):
+            before=current
+            current=current.next
+        if(current.value==value):
+            if(before):
+                before.next=current.next
+            else:
+                self.head=current.next            
+        
