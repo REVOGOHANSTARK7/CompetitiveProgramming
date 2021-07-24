562c8ddd0c12f2dfc7b9ecc8e16556cedc77f08c
REVOGOHANSTARK7 <72118470+REVOGOHANSTARK7@users.noreply.github.com>
2021-07-24 19:56:00 +0530

    Day11_2

--- 04-nth_carolprime-Python/nth_carolprime.py
+++ 04-nth_carolprime-Python/nth_carolprime.py
@@ -8,6 +8,29 @@
 # Hint: you may need to generate only Carol numbers, and then test those as you go 
 # for primality (and you may need to think about that hint for a while for it to make sense!).
 
+def isaprime(y):
+  if(y<2):
+    return False
+  if(y==2):
+    return True
+  if(y%2==0):
+    return False
+  maximfactor=round(y**0.5)
+  for m in range(3,maximfactor+1,2):
+    if(y%m==0):
+      return False
+  return True
+
 
 def fun_nth_carolprime(n):
-    return 0
+    k=2
+    found=0
+    while(found<=n):
+        x=(((2**k)-1)**2-2)
+        if(isaprime(x)):
+            found=found+1
+        k=k+1
+    # return x
+    print(x)
+    
+fun_nth_carolprime(3)
