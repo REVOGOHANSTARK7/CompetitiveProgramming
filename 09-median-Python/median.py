2dadd1cbffa26f3ec31bfd3f40fc9afc0e8965fb
REVOGOHANSTARK7 <72118470+REVOGOHANSTARK7@users.noreply.github.com>
2021-07-24 19:40:51 +0530

    Day11_1

--- 09-median-Python/median.py
+++ 09-median-Python/median.py
@@ -3,6 +3,15 @@
 # which is the value of the middle element, or the average of the two middle elements if there is no single middle 
 # element. If the list is empty, return None.
 
+# https://github.com/vprusso/youtube_tutorials/blob/master/data_structures/trees/binary_trees/binary_tree.py
+
 def median(a):
-	# your code goes here
-	pass
+  low=0
+  mid=len(a)//2
+  if(len(a)==0):
+    return None
+  elif((len(a)%2)!=0):
+    return a[(low+(len(a)-1))//2]
+  else:
+    return (a[mid]+a[~mid])/2
+  
