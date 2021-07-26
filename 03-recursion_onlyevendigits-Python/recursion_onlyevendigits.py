131bb754a03226f10eeef1e2322b3deca5481173
REVOGOHANSTARK7 <72118470+REVOGOHANSTARK7@users.noreply.github.com>
2021-07-26 19:59:39 +0530

    Day12_5

--- 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
+++ 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
@@ -8,5 +8,28 @@
 # Also the function returns the empty list if the original list is empty. 
 # Remember to not use strings. You may not use loops/iteration in this problem.
 
-def fun_recursion_onlyevendigits(l): 
-		return []
+def wrapper(n,i=0,null=0):
+  if(n==0):
+    return null
+  temp=n%10
+  if(temp%2==0):
+    null+=(10**i*temp)
+    i+=1
+  return wrapper(n//10,i,null)
+
+def fun_recursion_onlyevendigits(l):
+  empty=[]
+  if(len(l)==0):
+    return empty
+  else:
+    even=wrapper(l[0])
+    empty.append(even)
+    return empty+fun_recursion_onlyevendigits(l[1:])
+  
+  
+
+
+
+  
+  
+
