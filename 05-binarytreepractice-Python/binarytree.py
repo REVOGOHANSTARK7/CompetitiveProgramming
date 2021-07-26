8284007f277d0a0432f2046df653bcbad85463b8
REVOGOHANSTARK7 <72118470+REVOGOHANSTARK7@users.noreply.github.com>
2021-07-26 14:54:12 +0530

    sss

--- 05-binarytreepractice-Python/binarytree.py
+++ 05-binarytreepractice-Python/binarytree.py
@@ -26,8 +26,7 @@ class BinaryTree(object):
             if start.value == find_val:
                 return True
             else:
-                return self.preorder_search(start.left, find_val) or \
-                       self.preorder_search(start.right, find_val)
+                return self.preorder_search(start.left, find_val) or self.preorder_search(start.right, find_val)
         return False
         
         """
