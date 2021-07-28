07a044a2e78bfa769ec4c4c745636c65bcf5b750
REVOGOHANSTARK7 <72118470+REVOGOHANSTARK7@users.noreply.github.com>
2021-07-28 17:46:16 +0530

    Day13_5

--- 05-canqueenattack-Python/canqueenattack.py
+++ 05-canqueenattack-Python/canqueenattack.py
@@ -4,5 +4,11 @@
 # diagonally.
 
 def canqueenattack(qR, qC, oR, oC):
-	# Your code goes here
-	pass
+  if(qR==oR):
+    return True
+  elif(qC==oC):
+    return True
+  elif((qR-qC)==(oR-oC)):
+    return True
+  else:
+    return False
