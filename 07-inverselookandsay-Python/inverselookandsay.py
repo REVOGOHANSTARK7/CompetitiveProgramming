b3c6a072f09f9b1fb33833a18f3a430afb1359a9
REVOGOHANSTARK7 <72118470+REVOGOHANSTARK7@users.noreply.github.com>
2021-07-28 17:37:45 +0530

    Day13_3

--- 07-inverselookandsay-Python/inverselookandsay.py
+++ 07-inverselookandsay-Python/inverselookandsay.py
@@ -21,5 +21,16 @@
 # inverseLookAndSay([(2,3),(1,8),(4,3)]) == [3,3,8,3,3,3,3])
 
 def inverselookandsay(a):
-	# Your code goes here
-	pass
+  leng=len(a)
+  if(leng==0):
+    return []  
+  count=1  
+  empty=[] 
+  for singl in range(leng-1):
+    if(a[singl]==a[singl+1]):
+      count=count+1
+    else:
+      empty.append((count,a[singl]))
+      count=1
+  empty.append((a[leng-1]),count)
+  return empty
