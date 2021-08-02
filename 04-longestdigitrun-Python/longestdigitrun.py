841cce44d0546364f04ec34a62f4cc00a842ce19
REVOGOHANSTARK7 <72118470+REVOGOHANSTARK7@users.noreply.github.com>
2021-08-02 11:32:22 +0530

    Day16_2

--- 04-longestdigitrun-Python/longestdigitrun.py
+++ 04-longestdigitrun-Python/longestdigitrun.py
@@ -4,6 +4,31 @@
 # run, or the smallest such digit if there is a tie. So, longestDigitRun(117773732) returns 7 (
 # because there is a run of 3 consecutive 7's), 
 # as does longestDigitRun(-677886).
+
 def longestdigitrun(n):
-	# Your code goes here
-	pass
+  n=abs(n)
+  s=str(n)
+  max=0
+  prev=0
+  count=1
+  for i in range(len(s)-1):
+    if(s[i]==s[i+1]):
+      count+=1
+      if(count==prev  and  len(s)-2==i):
+        if(max>int(s[i])):
+          max=int(s[i])
+    else:
+      if(count==prev):
+        if(max>int(s[i])):
+          max=int(s[i])
+          prev=count
+          count=1
+      if(max<int(s[i]) and count>prev):
+        max=int(s[i])
+        prev=count
+      count=1
+  return max
+        
+#   print(max)
+# longestdigitrun(1177737888832)
+      
