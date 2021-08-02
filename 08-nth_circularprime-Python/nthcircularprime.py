cb39f4d4e273aa7b6c5f5235edafdea5f271da19
REVOGOHANSTARK7 <72118470+REVOGOHANSTARK7@users.noreply.github.com>
2021-08-02 19:45:50 +0530

    Day17_2

--- 08-nth_circularprime-Python/nthcircularprime.py
+++ 08-nth_circularprime-Python/nthcircularprime.py
@@ -5,6 +5,37 @@
 # 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97, 113, 131, 197... To see why 197 is a Circular prime, 
 # note that 197 is prime, as is 971 (rotated left), as is 719 (rotated left again).
 
+import math
+
+def isprime(n):
+  if(n<2):
+    return False
+  for i in range(2,n):
+    if(n%i==0):
+      return False
+  return True
+
+def circular(n):
+  count=0
+  temp=n
+  while(temp>0):
+    count+=1
+    temp//=10
+  temp2=n
+  while(isprime(temp2)):
+    rem=temp2%10
+    quo=temp2//10
+    temp2=int(((math.pow(10,(count-1)))*rem)+quo)
+    if(temp2==n):
+      return True
+  return False
+  
 def nthcircularprime(n):
-	# Your code goes here
-	pass
+  found=1
+  got=0
+  while(found<=n):
+    got+=1
+    if(circular(got)):
+      found+=1
+  return got
+
